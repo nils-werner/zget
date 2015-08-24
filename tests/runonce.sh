011cb389b210dfc5af88bdd4f396dece4f6cbc7e
Nils Werner <nils.werner@audiolabs-erlangen.de>
2015-08-24 15:35:26 +0200

    tests dont show a mess of verbosity and progress bar anymore

--- tests/runonce.sh
+++ tests/runonce.sh
@@ -1,4 +1,5 @@
 set -e
+set -o xtrace
 
 if [[ `uname` == 'Darwin' ]]; then
     IFACE='lo0'
@@ -6,20 +7,24 @@ else
     IFACE='lo'
 fi
 
-zput -vv -i $IFACE -t 3 LICENSE &
-zget -vv -t 3 LICENSE /dev/null
 
-zput -vv -t 3 -a 127.0.0.1 LICENSE &
-zget -vv -t 3 LICENSE /dev/null
+zput -vv -q -t 3 LICENSE &
+zget -vv -q -t 3 LICENSE /dev/null
 
-zput -vv -t 3 -p 8808 LICENSE &
-zget -vv -t 3 LICENSE /dev/null
+zput -vv -q -i $IFACE -t 3 LICENSE &
+zget -vv -q -t 3 LICENSE /dev/null
+
+zput -vv -q -t 3 -a 127.0.0.1 LICENSE &
+zget -vv -q -t 3 LICENSE /dev/null
+
+zput -vv -q -t 3 -p 8808 LICENSE &
+zget -vv -q -t 3 LICENSE /dev/null
 
 zput -q -t 3 LICENSE &
 zget -q -t 3 LICENSE /dev/null
 
-zput -vv -t 3 LICENSE &
-zget -vv -t 3 LICENSE /dev/null
+zput -t 3 LICENSE &
+zget -t 3 LICENSE /dev/null
 
-zput -vv -t 3 "tests/filename with spaces" &
-zget -vv -t 3 "filename with spaces" /dev/null
+zput -vv -q -t 3 "tests/filename with spaces" &
+zget -vv -q -t 3 "filename with spaces" /dev/null
