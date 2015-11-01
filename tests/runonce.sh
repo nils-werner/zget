91280c2463e4b5f9e652824a30afb2e9d85c3256
Nils Werner <nils.werner@gmail.com>
2015-11-01 15:12:28 +0100

    Less aggressive timeouts

--- tests/runonce.sh
+++ tests/runonce.sh
@@ -11,32 +11,32 @@ LANGS=( "de_DE" "en_US" )
 
 for LANG in "${LANGS[@]}"; do
     export LANGUAGE=$LANG
-    zput -vv -q -t 3 LICENSE &
-    zget -vv -q -t 3 LICENSE /dev/null
+    zput -vv -q -t 10 LICENSE &
+    zget -vv -q -t 10 LICENSE /dev/null
 
-    zput -vv -q -i $IFACE -t 3 LICENSE &
-    zget -vv -q -t 3 LICENSE /dev/null
+    zput -vv -q -i $IFACE -t 10 LICENSE &
+    zget -vv -q -t 10 LICENSE /dev/null
 
-    zput -vv -q -t 3 -a 127.0.0.1 LICENSE &
-    zget -vv -q -t 3 LICENSE /dev/null
+    zput -vv -q -t 10 -a 127.0.0.1 LICENSE &
+    zget -vv -q -t 10 LICENSE /dev/null
 
-    zput -vv -q -t 3 -p 8808 LICENSE &
-    zget -vv -q -t 3 LICENSE /dev/null
+    zput -vv -q -t 10 -p 8808 LICENSE &
+    zget -vv -q -t 10 LICENSE /dev/null
 
-    zput -q -t 3 LICENSE &
-    zget -q -t 3 LICENSE /dev/null
+    zput -q -t 10 LICENSE &
+    zget -q -t 10 LICENSE /dev/null
 
-    zput -t 3 LICENSE &
-    zget -t 3 LICENSE /dev/null
+    zput -t 10 LICENSE &
+    zget -t 10 LICENSE /dev/null
 
-    zput -vv -q -t 3 "tests/filename with spaces" &
-    zget -vv -q -t 3 "filename with spaces" /dev/null
+    zput -vv -q -t 10 "tests/filename with spaces" &
+    zget -vv -q -t 10 "filename with spaces" /dev/null
 
     # Transfer alias, requesting file
-    zput -vv -q -t 3 LICENSE asd &
-    zget -vv -q -t 3 LICENSE /dev/null
+    zput -vv -q -t 10 LICENSE asd &
+    zget -vv -q -t 10 LICENSE /dev/null
 
     # Transfer alias, requesting alias
-    zput -vv -q -t 3 LICENSE asd &
-    zget -vv -q -t 3 asd /dev/null
+    zput -vv -q -t 10 LICENSE asd &
+    zget -vv -q -t 10 asd /dev/null
 done
