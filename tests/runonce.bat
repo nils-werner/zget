69798f1fde301afbaead81ba0a929a4b06501c38
Nils Werner <nils.werner@gmail.com>
2016-12-05 10:45:17 +0100

    Windows /dev/null

--- tests/runonce.bat
+++ tests/runonce.bat
@@ -1,25 +1,25 @@
 START "" zput -vv -q -t 10 LICENSE
-zget -vv -q -t 10 LICENSE /dev/null
+zget -vv -q -t 10 LICENSE nul
 
 START "" zput -vv -q -t 10 -a 127.0.0.1 LICENSE
-zget -vv -q -t 10 LICENSE /dev/null
+zget -vv -q -t 10 LICENSE nul
 
 START "" zput -vv -q -t 10 -p 8808 LICENSE
-zget -vv -q -t 10 LICENSE /dev/null
+zget -vv -q -t 10 LICENSE nul
 
 START "" zput -q -t 10 LICENSE
-zget -q -t 10 LICENSE /dev/null
+zget -q -t 10 LICENSE nul
 
 START "" zput -t 10 LICENSE
-zget -t 10 LICENSE /dev/null
+zget -t 10 LICENSE nul
 
 START "" zput -vv -q -t 10 "tests/filename with spaces"
-zget -vv -q -t 10 "filename with spaces" /dev/null
+zget -vv -q -t 10 "filename with spaces" nul
 
 # Transfer alias, requesting file
 START "" zput -vv -q -t 10 LICENSE asd
-zget -vv -q -t 10 LICENSE /dev/null
+zget -vv -q -t 10 LICENSE nul
 
 # Transfer alias, requesting alias
 START "" zput -vv -q -t 10 LICENSE asd
-zget -vv -q -t 10 asd /dev/null
+zget -vv -q -t 10 asd nul
