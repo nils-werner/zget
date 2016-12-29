6f28ce98503d9f32ec24eee7b2a8f66ca60cf88a
Nils Werner <nils.werner@gmail.com>
2016-12-29 19:31:35 +0100

    MD5 checks on transmitted files

--- tests/runonce.sh
+++ tests/runonce.sh
@@ -3,8 +3,10 @@ set -o xtrace
 
 if [[ `uname` == 'Darwin' ]]; then
     IFACE='lo0'
+    md5="md5sum"
 else
     IFACE='lo'
+    md5="md5sum"
 fi
 
 LANGS=( "de_DE" "en_US" )
@@ -12,31 +14,40 @@ LANGS=( "de_DE" "en_US" )
 for LANG in "${LANGS[@]}"; do
     export LANGUAGE=$LANG
     zput -vv -q -t 10 LICENSE &
-    zget -vv -q -t 10 LICENSE /dev/null
+    zget -vv -q -t 10 LICENSE recv_LICENSE
+    eval $md5 -c tests/md5sum
 
     zput -vv -q -i $IFACE -t 10 LICENSE &
-    zget -vv -q -t 10 LICENSE /dev/null
+    zget -vv -q -t 10 LICENSE recv_LICENSE
+    eval $md5 -c tests/md5sum
 
     zput -vv -q -t 10 -a 127.0.0.1 LICENSE &
-    zget -vv -q -t 10 LICENSE /dev/null
+    zget -vv -q -t 10 LICENSE recv_LICENSE
+    eval $md5 -c tests/md5sum
 
     zput -vv -q -t 10 -p 8808 LICENSE &
-    zget -vv -q -t 10 LICENSE /dev/null
+    zget -vv -q -t 10 LICENSE recv_LICENSE
+    eval $md5 -c tests/md5sum
 
     zput -q -t 10 LICENSE &
-    zget -q -t 10 LICENSE /dev/null
+    zget -q -t 10 LICENSE recv_LICENSE
+    eval $md5 -c tests/md5sum
 
     zput -t 10 LICENSE &
-    zget -t 10 LICENSE /dev/null
+    zget -t 10 LICENSE recv_LICENSE
+    eval $md5 -c tests/md5sum
 
     zput -vv -q -t 10 "tests/filename with spaces" &
-    zget -vv -q -t 10 "filename with spaces" /dev/null
+    zget -vv -q -t 10 "filename with spaces" recv_LICENSE
 
     # Transfer alias, requesting file
     zput -vv -q -t 10 LICENSE asd &
-    zget -vv -q -t 10 LICENSE /dev/null
+    zget -vv -q -t 10 LICENSE recv_LICENSE
+    eval $md5 -c tests/md5sum
 
     # Transfer alias, requesting alias
     zput -vv -q -t 10 LICENSE asd &
-    zget -vv -q -t 10 asd /dev/null
+    zget -vv -q -t 10 asd recv_LICENSE
+    eval $md5 -c tests/md5sum
+    rm recv_LICENSE
 done
